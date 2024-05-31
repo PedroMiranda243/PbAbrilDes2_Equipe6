16d0f2baed7b8a96afb49c53eecfcba8e90aa2ca
Eric Avelino <ericfravelino@gmail.com>
2024-05-31 16:17:42 -0300

    Swagger documentation added on AuthenticationController and RegisterController, also updated init.sql with register table

--- src/main/java/com/example/twitter/demo/controller/UsuarioController.java
+++ src/main/java/com/example/twitter/demo/controller/UsuarioController.java
@@ -5,7 +5,14 @@ import com.example.twitter.demo.dto.RegisterResponseDto;
 import com.example.twitter.demo.dto.RegisterSenhaDto;
 import com.example.twitter.demo.dto.mapper.RegisterMapper;
 import com.example.twitter.demo.entity.Register;
+import com.example.twitter.demo.exception.ErrorMessage;
 import com.example.twitter.demo.service.RegisterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,30 +29,88 @@ public class UsuarioController {
     @Autowired
     private RegisterService registerService;
 
+    @Operation(
+    summary ="Criar um novo usuario", description = "Recurso de criação de usuário da API",
+        responses ={
+            @ApiResponse(responseCode = "201", description = "Usuário criado com sucesso",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = RegisterResponseDto.class))),
+            @ApiResponse(responseCode = "409", description = "email ja cadastrado no sistema",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            @ApiResponse(responseCode = "422", description = "O recurso processado por dados de entrada inválidos",
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     public ResponseEntity<RegisterResponseDto> create(@Valid @RequestBody RegisterCreateDto createDto) {
         Register register = registerService.salvar(RegisterMapper.toUsuario(createDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(RegisterMapper.toDto(register));
     }
 
+    //Campo comentado em espera de autorização do recurso
+    @Operation(
+            summary ="Recuperação de usuário por Id", description = "Recurso de recuperação de usuário por Id da API",
+            //security = @SecurityRequirement(name = "security"),
+            responses ={
+                    @ApiResponse(responseCode = "200", description = "Usuário encontrado com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = RegisterResponseDto.class))),
+                    @ApiResponse(responseCode = "403", description = "Usuário sem permissão para acessar este recurso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping("/{id}")
     public ResponseEntity<RegisterResponseDto> getById(@PathVariable Long id) {
         Register register = registerService.buscarPorId(id);
         return ResponseEntity.ok(RegisterMapper.toDto(register));
     }
 
+    //Campos comentados em espera de autorização do recurso
+    @Operation(
+            summary ="Atualização de senha", description = "Recurso de atualização de senha, recurso necessita de um token Bearer." +
+            "Acesso restrito a Admin|Cliente",
+            //security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Senha atualizada com sucesso"),
+                    @ApiResponse(responseCode = "400", description = "Senha não confere",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    //@ApiResponse(responseCode = "403", description = "Usuário sem permissão para acessar este recurso",
+                    //        content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Campos inválidos ou mal formatados",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id, @Valid @RequestBody RegisterSenhaDto dto) {
         registerService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmaSenha());
         return ResponseEntity.noContent().build();
     }
 
+    //Campos comentado em espera de autorização do recurso
+    @Operation(summary = "Listar todos os usuários cadastrados", description = "Requisição exige um Bearer Token. Acesso restrito a ADMIN",
+            //security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Lista com todos os usuários cadastrados",
+                            content = @Content(mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = RegisterResponseDto.class))))
+                    //,@ApiResponse(responseCode = "403", description = "Usuário sem permissão para acessar este recurso",
+                            //content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @GetMapping
     public ResponseEntity<List<RegisterResponseDto>> getAll() {
         List<Register> registers = registerService.buscarTodos();
         return ResponseEntity.ok(RegisterMapper.toListDto(registers));
     }
 
+    //Campos comentados em espera de autorização do recurso
+    @Operation(
+            summary ="Exclusão de usuário por Id", description = "Recurso de exclusão de usuário por Id da API." +
+            "Requisição exige um Bearer Token. Acesso restrito a ADMIN",
+            //security = @SecurityRequirement(name = "security"),
+            responses ={
+                    @ApiResponse(responseCode = "204", description = "Usuário deletado com sucesso"),
+                    //@ApiResponse(responseCode = "403", description = "Usuário sem permissão para acessar este recurso",
+                    //        content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Recurso não encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteById(@PathVariable Long id) {
         registerService.excluirPorId(id);
